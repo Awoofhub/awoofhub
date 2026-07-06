5a1cacb0ec27c5a45a6321524a17a5245d4839f1
divine-itura <divine@myitura.com>
2026-07-06 10:56:09 +0100

    fixed merges

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import { Stack } from "expo-router";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-export default function AuthLayout() {
-
-  return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-    <Stack>
-      <Stack.Screen name="login" options={{ title: 'Login' , headerShown: false}} />
-      <Stack.Screen name="signup" options={{ title: 'Sign Up' ,headerShown: false}} />
-    </Stack>
-    </GestureHandlerRootView>
-  );
-}
-=======
 import { Stack } from "expo-router";
 
 export default function AuthLayout() {
@@ -24,4 +9,3 @@ export default function AuthLayout() {
     </Stack>
   );
 }
->>>>>>> development
